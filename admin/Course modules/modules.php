09c854af02dfb66bbb4889e9c49ef7577dc35df2
AbhithaVish <162031145+AbhithaVish@users.noreply.github.com>
2024-06-15 21:12:43 +0530

    Course modules searching extra file removed

--- admin/Course modules/modules.php
+++ admin/Course modules/modules.php
@@ -100,7 +100,6 @@ if ($stmt) {
             <table class="table">
                 <thead>
                     <tr>
-                        <th>Course</th>
                         <th>Module Name</th>
                         <th>Module Code</th>
                         <th>Date</th>
@@ -113,7 +112,6 @@ if ($stmt) {
                 <tbody>
                     <?php foreach ($modules as $module): ?>
                         <tr>
-                            <td><?php echo htmlspecialchars($module['course']); ?></td>
                             <td><?php echo htmlspecialchars($module['module_name']); ?></td>
                             <td><?php echo htmlspecialchars($module['module_code']); ?></td>
                             <td><?php echo htmlspecialchars($module['date']); ?></td>
